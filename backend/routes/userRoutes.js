d4e398fcfee0cdc2d64cd2662d24c21af89490be
saitejamandadi99 <saitejamandadi99>
2025-09-30 16:51:07 +0530

    added new routes for filtered user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express'); 
 const router = express.Router()
 
-const {getAllUsers, deleteUserById, createUser, updateUserById} = require('../controllers/userControllers')
+const {getAllUsers,getUsers, deleteUserById, createUser, updateUserById} = require('../controllers/userControllers')
 
 router.get('/users', getAllUsers)
+router.post('/get-users',getUsers )
 router.post('/users', createUser)
 router.delete('/users/:id', deleteUserById)
 router.put('/users/:id', updateUserById)
 
-module.exports = router 
+module.exports = router     
