5a69868fb1030996619a19e4441e683e7046a32b
saitejamandadi99 <saitejamandadi99>
2025-09-30 13:35:13 +0530

    created a user table to store the info of users

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const db = require('./db/db')
+const userTable = () =>{
+    const sql = `
+    create table if not exists user(
+        user_id text primary key, 
+        full_name text not null, 
+        mobile_no text not null, 
+        pan_num text not null, 
+        manager_id integer, 
+        foreign key (manager_id) references manager(id)) 
+    `
+
+    db.run(sql, (err)=>{
+        if (err){
+            console.error('error while creating the user table',err.message)
+        }
+        console.log('User table got created successfully')
+    })
+}
+
+module.exports = userTable
