88babf767d7fe249d5ee3116d2006295df27ff9a
saitejamandadi99 <saitejamandadi99>
2025-09-30 15:43:36 +0530

    created controlled for delete user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,5 @@
 const db = require('../db/db')
-
+//get all users
 const getAllUsers = async (req , res )=>{
     try{
         const sql = `select * from user`
@@ -18,3 +18,32 @@ const getAllUsers = async (req , res )=>{
         res.status(500).json({message:error.message})
     }
 }
+
+//delete user by id
+
+const deleteUserById = (req , res) => {
+    const userId = req.params.id 
+    try{
+        const sql = `select * from user where user_ id = ?`
+        db.get(sql, [userId], (err, row)=>{
+            if(err){
+                throw err
+            }
+            if(!row){
+                return res.status(404).json({message:`user with the id ${userId} not found`})
+            }
+            const deleteSql = `delete from user where id = ?`
+            db.run(deleteSql, [userId], (err)=>{
+                if(err){
+                    throw err
+                }
+                res.status(200).json({message:`user with the id ${userId} deleted successfully`})
+            })
+        
+        })
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+}
+
